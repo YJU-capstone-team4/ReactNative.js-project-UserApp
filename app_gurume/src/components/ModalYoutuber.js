ca04c6b6ea58aa49b14bc4eb9232e19d05bddd73
SeungHyun Cho <c0305sh@naver.com>
2021-01-29 13:52:07 +0900

    Update :: Screen - ModalYoutuber ScrollView StickyHeader Setting

--- app_gurume/src/components/ModalYoutuber.js
+++ app_gurume/src/components/ModalYoutuber.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import { StyleSheet, View, ScrollView, TouchableOpacity, Image, Dimensions } from 'react-native'
+import { StyleSheet, View, ScrollView, TouchableOpacity, Image, Dimensions, TextInput } from 'react-native'
 
 // import styles
-import { Colors } from '@styles';
+import { Colors, Typography } from '@styles';
 import { getStatusBarHeight } from "react-native-status-bar-height";
 import { Text } from './../styles/CommonStyles';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
@@ -25,10 +25,8 @@ const ModalYoutuber = (props) => {
     }
 
     return (
-        // props.modalVisible ?
-        <ScrollView style={styles.container}>
+        <View style={styles.container}>
             <View style={styles.headerContainer}>
-                {/* <Text style={styles.backBtnContainer}>뒤로가기</Text> */}
                 <TouchableOpacity
                     onPress={() => toggleBackButton()}
                     hitSlop={{ top: 20, right: 20, bottom: 20, left: 20 }}
@@ -38,93 +36,100 @@ const ModalYoutuber = (props) => {
                 </TouchableOpacity>
                 <Text size={22} weight="BOLD">검색결과</Text>
             </View>
-            {/* 재검색 컴포넌트 */}
-            <View>
-            </View>
-            {/* 검색 결과 반영 */}
-            <View style={{ marginTop: 20, padding: 13, marginHorizontal: 20, justifyContent: 'center', alignItems: 'center', borderWidth: 0.5, borderColor: Colors.GRAY_8, borderRadius: 50, flexDirection: 'row' }}>
-                <Text style={{ textAlign: 'center' }} size={20}>맛집 정보가 궁금해?</Text>
-                <MaterialCommunityIcons style={{ position: 'absolute', right: 20 }} name="magnify" size={24} />
-            </View>
-            <View style={[styles.wrapperContainer, { justifyContent: 'center', borderWidth: 0 }]}>
-                <Text weight="BOLD">야식이</Text>
-                <Text> 관련 데이터가 3건 있습니다.</Text>
-            </View>
-            {/* 유튜버 리스트 나열 */}
-            <View style={styles.wrapperContainer}>
-                <Image style={styles.youtuberImage} source={thumb} />
-                <View style={{ paddingLeft: 20 }}>
-                    <Text style={{ paddingTop: 30 }} size={22}>문복희 Eat with Boki</Text>
-                    <Text style={{ paddingVertical: 10 }}>구독자 200K</Text>
-                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                        <Text>다녀간 맛집 정보 </Text>
-                        <Text color={Colors.RED_4} size={20}>30</Text>
-                        <Text> 건</Text>
+            <ScrollView style={{ flex: 1 }} stickyHeaderIndices={[1]}>
+                {/* 검색 컴포넌트 */}
+                <View style={styles.searchContainer}>
+                    <TextInput style={styles.searchText} placeholder="맛집 정보가 궁금해?" />
+                    {/* 검색 아이콘 */}
+                    <TouchableOpacity
+                        style={{ position: 'absolute', right: 20 }}
+                        hitSlop={{ top: 20, right: 20, bottom: 20, left: 20 }}
+                    >
+                        <MaterialCommunityIcons name="magnify" size={24} />
+                    </TouchableOpacity>
+                </View>
+                {/* 검색 결과 반영 */}
+                <View>
+                    <View style={[styles.wrapperContainer, styles.stickyWrapper]}>
+                        <Text weight="BOLD">야식이</Text>
+                        <Text> 관련 데이터가 3건 있습니다.</Text>
                     </View>
                 </View>
-            </View>
-            <View style={styles.wrapperContainer}>
-                <Image style={styles.youtuberImage} source={thumb} />
-                <View style={{ paddingLeft: 20 }}>
-                    <Text style={{ paddingTop: 30 }} size={22}>문복희 Eat with Boki</Text>
-                    <Text style={{ paddingVertical: 10 }}>구독자 200K</Text>
-                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                        <Text>다녀간 맛집 정보 </Text>
-                        <Text color={Colors.RED_4} size={20}>30</Text>
-                        <Text> 건</Text>
+                {/* 유튜버 리스트 나열 */}
+                <View style={styles.wrapperContainer}>
+                    <Image style={styles.youtuberImage} source={thumb} />
+                    <View style={{ paddingLeft: 20 }}>
+                        <Text style={{ paddingTop: 30 }} size={22}>문복희 Eat with Boki</Text>
+                        <Text style={{ paddingVertical: 10 }}>구독자 200K</Text>
+                        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                            <Text>다녀간 맛집 정보 </Text>
+                            <Text color={Colors.RED_4} size={20}>30</Text>
+                            <Text> 건</Text>
+                        </View>
                     </View>
                 </View>
-            </View>
-            <View style={styles.wrapperContainer}>
-                <Image style={styles.youtuberImage} source={thumb} />
-                <View style={{ paddingLeft: 20 }}>
-                    <Text style={{ paddingTop: 30 }} size={22}>문복희 Eat with Boki</Text>
-                    <Text style={{ paddingVertical: 10 }}>구독자 200K</Text>
-                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                        <Text>다녀간 맛집 정보 </Text>
-                        <Text color={Colors.RED_4} size={20}>30</Text>
-                        <Text> 건</Text>
+                <View style={styles.wrapperContainer}>
+                    <Image style={styles.youtuberImage} source={thumb} />
+                    <View style={{ paddingLeft: 20 }}>
+                        <Text style={{ paddingTop: 30 }} size={22}>문복희 Eat with Boki</Text>
+                        <Text style={{ paddingVertical: 10 }}>구독자 200K</Text>
+                        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                            <Text>다녀간 맛집 정보 </Text>
+                            <Text color={Colors.RED_4} size={20}>30</Text>
+                            <Text> 건</Text>
+                        </View>
                     </View>
                 </View>
-            </View>
-            <View style={styles.wrapperContainer}>
-                <Image style={styles.youtuberImage} source={thumb} />
-                <View style={{ paddingLeft: 20 }}>
-                    <Text style={{ paddingTop: 30 }} size={22}>문복희 Eat with Boki</Text>
-                    <Text style={{ paddingVertical: 10 }}>구독자 200K</Text>
-                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                        <Text>다녀간 맛집 정보 </Text>
-                        <Text color={Colors.RED_4} size={20}>30</Text>
-                        <Text> 건</Text>
+                <View style={styles.wrapperContainer}>
+                    <Image style={styles.youtuberImage} source={thumb} />
+                    <View style={{ paddingLeft: 20 }}>
+                        <Text style={{ paddingTop: 30 }} size={22}>문복희 Eat with Boki</Text>
+                        <Text style={{ paddingVertical: 10 }}>구독자 200K</Text>
+                        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                            <Text>다녀간 맛집 정보 </Text>
+                            <Text color={Colors.RED_4} size={20}>30</Text>
+                            <Text> 건</Text>
+                        </View>
                     </View>
                 </View>
-            </View>
-            <View style={styles.wrapperContainer}>
-                <Image style={styles.youtuberImage} source={thumb} />
-                <View style={{ paddingLeft: 20 }}>
-                    <Text style={{ paddingTop: 30 }} size={22}>문복희 Eat with Boki</Text>
-                    <Text style={{ paddingVertical: 10 }}>구독자 200K</Text>
-                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                        <Text>다녀간 맛집 정보 </Text>
-                        <Text color={Colors.RED_4} size={20}>30</Text>
-                        <Text> 건</Text>
+                <View style={styles.wrapperContainer}>
+                    <Image style={styles.youtuberImage} source={thumb} />
+                    <View style={{ paddingLeft: 20 }}>
+                        <Text style={{ paddingTop: 30 }} size={22}>문복희 Eat with Boki</Text>
+                        <Text style={{ paddingVertical: 10 }}>구독자 200K</Text>
+                        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                            <Text>다녀간 맛집 정보 </Text>
+                            <Text color={Colors.RED_4} size={20}>30</Text>
+                            <Text> 건</Text>
+                        </View>
                     </View>
                 </View>
-            </View>
-            <View style={styles.wrapperContainer}>
-                <Image style={styles.youtuberImage} source={thumb} />
-                <View style={{ paddingLeft: 20 }}>
-                    <Text style={{ paddingTop: 30 }} size={22}>문복희 Eat with Boki</Text>
-                    <Text style={{ paddingVertical: 10 }}>구독자 200K</Text>
-                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                        <Text>다녀간 맛집 정보 </Text>
-                        <Text color={Colors.RED_4} size={20}>30</Text>
-                        <Text> 건</Text>
+                <View style={styles.wrapperContainer}>
+                    <Image style={styles.youtuberImage} source={thumb} />
+                    <View style={{ paddingLeft: 20 }}>
+                        <Text style={{ paddingTop: 30 }} size={22}>문복희 Eat with Boki</Text>
+                        <Text style={{ paddingVertical: 10 }}>구독자 200K</Text>
+                        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                            <Text>다녀간 맛집 정보 </Text>
+                            <Text color={Colors.RED_4} size={20}>30</Text>
+                            <Text> 건</Text>
+                        </View>
                     </View>
                 </View>
-            </View>
-        </ScrollView>
-        // : null
+                <View style={styles.wrapperContainer}>
+                    <Image style={styles.youtuberImage} source={thumb} />
+                    <View style={{ paddingLeft: 20 }}>
+                        <Text style={{ paddingTop: 30 }} size={22}>문복희 Eat with Boki</Text>
+                        <Text style={{ paddingVertical: 10 }}>구독자 200K</Text>
+                        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                            <Text>다녀간 맛집 정보 </Text>
+                            <Text color={Colors.RED_4} size={20}>30</Text>
+                            <Text> 건</Text>
+                        </View>
+                    </View>
+                </View>
+            </ScrollView>
+        </View>
     )
 }
 
@@ -153,17 +158,40 @@ const styles = StyleSheet.create({
         paddingVertical: 5,
         paddingHorizontal: 15
     },
+    searchContainer: {
+        flexDirection: 'row',
+        marginTop: 20,
+        padding: 10,
+        marginHorizontal: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderWidth: 0.5,
+        borderColor: Colors.GRAY_8,
+        borderRadius: 50,
+    },
+    searchText: {
+        textAlign: 'center',
+        fontSize: 15,
+        fontFamily: Typography.FONT_FAMILY_REGULAR
+    },
     backBtnContainer: {
         position: 'absolute',
         left: 20,
-        top: 25
+        top: 22
     },
     youtuberImage: {
-        width: width * 0.35,
-        height: width * 0.35,
+        width: width * 0.33,
+        height: width * 0.33,
         borderRadius: 100,
         borderColor: Colors.GRAY_7,
         borderWidth: 0.1
+    },
+    stickyWrapper: {
+        justifyContent: 'center',
+        borderWidth: 0,
+        marginTop: 0,
+        paddingTop: 10,
+        backgroundColor: Colors.WHITE,
     }
 
 })
