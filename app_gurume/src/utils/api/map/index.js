49e862d5fa29e0ca381cb7294f483fe2ecfd2eb9
SeungHyun Cho <c0305sh@naver.com>
2021-02-02 20:59:40 +0900

    Feature :: Hooks - useAsync + Custom Axios

---
+++ app_gurume/src/utils/api/map/index.js
@@ -0,0 +1,26 @@
+/**
+ * MAP SCREEN 관련 API 리스트 입니다.
+ */
+
+// import modules
+import { instance, afterAuth } from '../index'
+
+const DEFAULT_YOUTUBER_ID = '5fb73d0e4c2de82830b54834'
+
+// 🎃 API 리스트 🎃
+
+// 처음 로딩시 전체 마커 불러오기
+async function getAllMarkers() {
+    const response = await instance.get('map')
+    return response.data
+}
+
+// 선택한 유튜버의 마커만 불러오기
+async function getYoutuberMarkers(ytbChannel_id = DEFAULT_YOUTUBER_ID) {
+    const response = await instance.get(`/map/youtuberSearch/youtuber/${ytbChannel_id}`)
+    return response.data
+}
+
+
+
+export { getAllMarkers, getYoutuberMarkers }
